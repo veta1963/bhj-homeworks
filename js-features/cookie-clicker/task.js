86a909fb015c6f5365d3286807b3aed1aa60d8ca
veta1963 <104830918+veta1963@users.noreply.github.com>
2023-03-18 23:09:09 +0500

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,8 @@
-let clicker__counter = 0;
-
-document.getElementById("cookie").onclick = Counter;
-function Counter() {
-  document.getElementById("cookie").width = "300";
-  clicker__counter ++
-  document.getElementById("clicker__counter").innerHTML = clicker__counter;
+let clickerCounter = 0;
+document.getElementById("cookie").onclick = function () {
+	if (document.getElementById("cookie").width === 300)
+	document.getElementById("cookie").width = "200"
+	else document.getElementById("cookie").width = "300";	
+	clickerCounter ++;
+	document.getElementById("clicker__counter").innerHTML = clickerCounter;
 };
